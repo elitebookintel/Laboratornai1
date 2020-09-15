7f30ca67686e55986f2fd6206b02c22d5604d41d
elitebookintel <elitebookinteli5@gmail.com>
2020-09-15 10:55:04 +0300

    master

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main() 
 { 
-    cout << "Hello, world!" << endl;
+    cout << "Hello, world!" << endl;       cout << "vetka5!" << endl;
+
+	cout << "vetka1!" << endl;
+	cout << "vetka2!" << endl;
+	
+	cout << "Vetka3!" << endl;
+
+
+
+
+
+cout << "vetka4!" << endl;
     return 0; 
 }
